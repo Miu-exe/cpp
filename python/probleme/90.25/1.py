116d8e1724ea4aebfce47dff932eb9b2910cf0fb
Miu-exe <miu683198979337@gmail.com>
2026-09-25 08:57:12 +0300

    initial

---
+++ python/probleme/90.25/1.py
@@ -0,0 +1,12 @@
+x = 10
+y = 3.14
+nume  = "maria"
+ok=True
+print(type(x)) #scoate tipul lui x
+varsta = 16
+nume ="ana"
+print(f"Numele meu este {nume} si am {varsta} ani")
+
+a=int(input("a = ")) #citeste de tip int si afiseaza inainte a = (aci tastez eu)
+b=float(input("b = "))
+print(f"suma este {int(a+b)}")
